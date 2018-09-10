f217ecaeb828b48bc9e7f8a7cffc5a91bcf1c1b8
Shun Yao <ysmiracle@hotmail.com>
2018-09-10 14:02:32 -0400

    done

--- src/components/UserInput.js
+++ src/components/UserInput.js
@@ -1,13 +1,61 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class UserInput extends Component {
+  state = {
+    username: '',
+    hometown: '',
+  }
+
+  handleChange = event => {
+    this.setState({
+      [event.target.name]: event.target.value,
+    })
+  }
+
+  handleSubmit = event => {
+    event.preventDefault();
+    this.props.addUser(this.state);
+    this.setState({
+      username: '',
+      hometown: '',
+    })
+  }
+
   render() {
     return(
       <div>
-        user input
+        <form onSubmit={this.handleSubmit}>
+          Username:
+          <input type="text" value={this.state.username} name="username" onChange={this.handleChange}/>
+          Hometown:
+          <input type="text" value={this.state.hometown} name="hometown" onChange={this.handleChange}/>
+          <input type="submit" value="Submit"/>
+        </form>
+        <br />
+        <h3>All Users:</h3>
+        <ul>{this.props.users}</ul>
       </div>
     );
   }
 };
 
-export default UserInput;
+const mapStateToProps = state => {
+  return {
+    users: state.users.map(user => <li>{user.username}</li>),
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    addUser: (state) => dispatch({
+      type: 'ADD_USER', 
+      user: {
+        username: state.username, 
+        hometown: state.hometown,
+      }
+    })
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(UserInput);
--- src/reducers/manageUsers.js
+++ src/reducers/manageUsers.js
@@ -1,2 +1,8 @@
-export default function manageUsers(state, action){
+export default function manageUsers(state = { users: [] }, action){
+    switch(action.type) {
+        case 'ADD_USER':
+            return {users: [...state.users, action.user]};
+        default:
+            return state;
+    }
 }
